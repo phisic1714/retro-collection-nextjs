5fde88e007b71bc4f5cc0d2ee224f5685531a0c6
Peerapat Sariman <96192069+phisic1714@users.noreply.github.com>
2024-09-06 15:39:05 +0700

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,28 @@ import { Nostalgist } from 'nostalgist'
 
 const getGameData = async () => {
   // try {
-    const res = await fetch(process.env.URL+"/database", { cache: "no-store" })
-    return res.json()
+  const res = await fetch(process.env.URL + "/database", { cache: "no-store" })
+  return res.json()
   // } catch (error) {
-    // console.log(error)
+  // console.log(error)
   // }
 }
 
 export default async function Home() {
-
-  const Game  = await getGameData()
-  console.log(Game)
+  const Game = await getGameData()
   return (
     <main>
+      <div className="flex flex-row grid grid-cols-4 gap-4 flex items-center">
 
-      <>{Game.map((t:any) => (
-        <div className="flex flex-row grid grid-cols-4 gap-4">
-          <div className="border border-current	 rounded-lg shadow-2xl">{t.Title}</div>
+        <>{Game.map((t: any) => (
+          <div className="border border-current	 rounded-lg shadow-2xl text-center">
+            <img src={t.ImageURL} alt="Game Cover" className="object-contain  h-48 w-96"/>
+            {t.Title}</div>
 
-        </div>
-      ))}
-      </>
+        ))}
+        </>
+      </div>
+
     </main>
   );
 }
